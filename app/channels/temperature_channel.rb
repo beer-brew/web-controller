13ef5bcf399531b2a29277c002b698483377efcc
Qingbo <qingbo.zhang@rea-group.com>
2016-02-17 08:13:11 +0800

    it need subscript

--- app/channels/temperature_channel.rb
+++ app/channels/temperature_channel.rb
@@ -1,4 +1,3 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class TemperatureChannel < ApplicationCable::Channel
   def subscribed
     stream_from "temperature_channel"
