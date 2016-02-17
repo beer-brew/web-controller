a85f572c2de7e95461b7fcd53895f405c136a7b5
Qingbo <qingbo.zhang@rea-group.com>
2016-02-17 22:37:09 +0800

    modify dashboard for mobile

--- app/helpers/status_helper.rb
+++ app/helpers/status_helper.rb
@@ -1,2 +1,11 @@
 module StatusHelper
+  def color(t)
+    red = [0,[(t/0.390625).to_i,256].min].max
+    blue = [0,[(256-red).to_i,256].min].max
+    "\##{red.to_s(16)}00#{blue.to_s(16)}"
+  end
+
+  def temp_height(t)
+    "#{[25,[0, (0.25*t).to_i].max].min}px"
+  end
 end
