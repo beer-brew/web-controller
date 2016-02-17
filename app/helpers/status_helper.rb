5726df89a80bcbff9bd473cc8251115d61a063a3
Qingbo <qingbo.zhang@rea-group.com>
2016-02-17 22:56:09 +0800

    really fix decimal convert to hex color

--- app/helpers/status_helper.rb
+++ app/helpers/status_helper.rb
@@ -2,7 +2,7 @@ module StatusHelper
   def color(t)
     red = [0,[(t/0.390625).to_i,255].min].max
     blue = [0,[(255-red).to_i,255].min].max
-    "\##{red.to_s(16)}00#{blue.to_s(16)}"
+    "\##{"%02x" % red}00#{"%02x" % blue}"
   end
 
   def temp_height(t)
