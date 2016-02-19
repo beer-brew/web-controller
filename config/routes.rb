3a6e3fd8c7dd755d825d3cf8303ce1188f8f8cf3
Qingbo <qingbo.zhang@rea-group.com>
2016-02-19 17:49:19 +0800

    add link between two pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'stages/index'
+  get 'stages/index', as: 'stage'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'status#index'
