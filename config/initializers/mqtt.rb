954582256b6ac407e1bab538369351509ebbeade
Qingbo <qingbo.zhang@rea-group.com>
2016-05-18 12:35:44 +0800

    use system mqtt client

--- config/initializers/mqtt.rb
+++ config/initializers/mqtt.rb
@@ -1,7 +1,5 @@
 Thread.new do
-  MQTT::Client.connect(host: 'm10.cloudmqtt.com', port: '12948', username: 'qegbzezg', password: 'GaD8XPkgNZMH') do |c|
-    c.get('/temperature') do |topic,message|
-      TemperatureUpdateJob.perform_later(JSON.parse(message))
-    end
+  Rails.application.config.mqtt_client.get('/register') do |topic,message|
+    TemperatureUpdateJob.perform_later(JSON.parse(message))
   end
 end
