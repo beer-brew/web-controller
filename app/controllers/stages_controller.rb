6051e3d906ad8d96ef6f186198c20b4b8285ab9d
Qingbo <ripple0328@gmail.com>
2016-05-21 22:07:48 +0800

    use local variable instead of intance var to render partial

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,7 +1,8 @@
 class StagesController < ApplicationController
   def index
     @stages = Stage.all
-    @devices = Device.all
+    @input_devices = Device.wired_with_type('input')
+    @output_devices = Device.wired_with_type('output')
     @current_stage = Ongoing.first.try(:stage)
   end
 end
--- app/jobs/stage_broadcast_job.rb
+++ app/jobs/stage_broadcast_job.rb
@@ -7,6 +7,13 @@ class StageBroadcastJob < ApplicationJob
 
   private
   def render_stage(stage)
-    ApplicationController.renderer.render(partial: 'stages/stage', locals: {stage: stage})
+    ApplicationController.renderer.render(partial: 'stages/stage',
+                                          locals: {
+                                            stage: stage,
+                                            input_devices: Device.wired_with_type('input'),
+                                            output_devices: Device.wired_with_type('output'),
+                                            current: Ongoing.first.stage
+                                          }
+                                          )
   end
 end
