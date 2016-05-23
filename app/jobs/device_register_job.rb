fa66bd3a5b65630aef5656b3ae3ea36465f3299a
Qingbo <ripple0328@gmail.com>
2016-05-23 17:51:59 +0800

    use proper sytax for find and create by

--- app/jobs/device_register_job.rb
+++ app/jobs/device_register_job.rb
@@ -2,15 +2,16 @@ class DeviceRegisterJob < ApplicationJob
   queue_as :default
   
   def perform(msg)
-    d = Device.where(chip_id: msg['id']).first_or_create do |d| 
+    d = Device.find_or_create_by(chip_id: msg['id']) do |d| 
       d.ip = msg['ip']
       d.mac_address = msg['mac']
       d.ssid = msg['ssid']
       d.status = :online
-      d.pins.build(pin_number: 2)
     end
+    d.pins.create(pin_number: 2)
     ActionCable.server.broadcast 'register_channel', id: d.id, body: render_body(d) 
   end
+
   private
   def render_body(device)
     ApplicationController.renderer.render(partial: 'device_manage/device', locals: {device: device})
